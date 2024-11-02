3a2e1a10150c793238af4418f617b26b4690716a
Сафиуллин Самат <samatsafiullin7@gmail.com>
2024-11-02 10:02:29 +0300

    Добавьте файлы проекта.

---
+++ Apiapp/Controllers/HabitsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Apiapp.Controllers
+{
+    public class HabitsController : Controller
+    {
+        [ApiController]
+        [Route("[controller]")]
+        // GET: HabitsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: HabitsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Apiapp/Habits.cs
@@ -0,0 +1,10 @@
+﻿namespace Apiapp
+{
+    public class Habits
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartHabits { get; set; }
+        public int ProcentDone { get; set; }
+    }
+}
